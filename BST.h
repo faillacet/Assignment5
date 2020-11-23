0b4cea0da188a8544f6cad70a1ed948009da4ca9
cadrec <47367944+cadrec@users.noreply.github.com>
2020-11-23 12:00:03 -0800

    Add files via upload

---
+++ BST.h
@@ -0,0 +1,132 @@
+#include "TreeNode.h"
+
+using namespace std;
+
+template <class T>
+BST<T>::BST() //default constructor
+{
+  root = NULL;  //empty tree
+}
+
+template <class T>
+BST<T>::~BST()
+{
+  //iterate and delete
+  //need to add here
+}
+
+template <class T>
+void BST<T>::printTree()
+{
+  recPrint(root);
+}
+
+template <class T>
+void BST<T>::recPrint(TreeNode<T> *node)
+{
+  if (node==NULL)
+    return;
+  else{
+    recPrint(node->left);
+    cout << node->key.printInfo() << endl;
+    recPrint(node->right);
+  }
+}
+
+template <class T>
+bool BST<T>::isEmpty()
+{
+  return (root == NULL);
+}
+
+template <class T>
+void BST<T>::insert(T &value)
+{
+  // check if value exists, use contains(), if DNE then continue
+  TreeNode<T> *node = new TreeNode<T>(value);   //key is now set to  value
+  if(isEmpty())   //empty tree
+  {
+    root = node;
+  }
+  else    //not an empty tree, need to find insertion point
+  {
+    TreeNode<T> *current = root;
+    TreeNode<T> *parent;   //empty node
+
+    while(true)
+    {
+      parent = current;
+
+      if (node->data < current->data)
+      {
+        current = current->left;   //going left
+
+        if(current==NULL)  //we found the insertion point for new node
+        {
+          parent->left = node;
+          break;
+        }
+        //else keep looping
+      }
+      else //going right
+      {
+        current = current->right;
+
+        if(current==NULL)
+        {
+          parent->right = node;
+          break;
+        }
+      }
+    }
+  }
+}
+
+template <class T>
+bool BST<T>::getRoot()
+{
+  return root;
+}
+
+template <class T>
+bool BST<T>::contains(int value)
+{
+  if(isEmpty())
+    return false;
+  else  //not empty tree
+  {
+    TreeNode<T> *current = root;
+
+    while(current->data != value)
+    {
+      if(value < current->data)  //going left
+        current = current->left;
+      else
+        current = current->right;
+      if(current == NULL)   //we did not find it, DNE
+        return false;
+    }
+    cout << current->key.printInfo() << endl;
+  }
+  return true;
+}
+
+
+
+template <class T>
+void BST<T>::fileTree()
+{
+  recFile(root);
+}
+
+template <class T>
+void BST<T>::recFile(TreeNode<T> *node)
+{
+  if (node==NULL)
+    return;
+  else{
+    recFile(node->left);
+    file_ << node->key.printInfo() << endl;
+    recFile(node->right);
+  }
+}
